cc267c5e4b0b5cf106c6280b4ec64e16432630d1
y-sakurai <235877267+y-sakurai-code@users.noreply.github.com>
2025-11-16 06:08:26 +0000

    応用14章 finish

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get '/homes/about', to: 'homes#about', as: 'about'
+  resources :users, only: [:show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
