86f10c8dd9fd0f2428eece9fd21f4c2a2c8c8684
y-sakurai <235877267+y-sakurai-code@users.noreply.github.com>
2025-12-12 12:35:43 +0000

    fixes/admin_4

--- db/migrate/20251211114431_add_devise_to_admins.rb
+++ db/migrate/20251211114431_add_devise_to_admins.rb
@@ -4,15 +4,15 @@ class AddDeviseToAdmins < ActiveRecord::Migration[6.1]
   def self.up
     change_table :admins do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      ##t.string :email,              null: false, default: ""
+      ##t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      ##t.string   :reset_password_token
+      ##t.datetime :reset_password_sent_at
 
       ## Rememberable
-      t.datetime :remember_created_at
+      ##t.datetime :remember_created_at
 
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
@@ -37,8 +37,8 @@ class AddDeviseToAdmins < ActiveRecord::Migration[6.1]
       # t.timestamps null: false
     end
 
-    add_index :admins, :email,                unique: true
-    add_index :admins, :reset_password_token, unique: true
+    ##add_index :admins, :email,                unique: true
+    ##add_index :admins, :reset_password_token, unique: true
     # add_index :admins, :confirmation_token,   unique: true
     # add_index :admins, :unlock_token,         unique: true
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_12_10_115556) do
+ActiveRecord::Schema.define(version: 2025_12_11_114431) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
